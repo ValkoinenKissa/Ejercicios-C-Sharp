a047c53f78586478d68139e913514d11c91ae284
Alberto Bollo <albertobollo7@protonmail.com>
2024-10-29 22:00:12 +0100

    Actualización ejercicios

---
+++ Ejercicios/Primer_Trimestre/Introduce_Datos/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Usuario introduce tu nombre: ");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Tu nombre es: " + nombre);
