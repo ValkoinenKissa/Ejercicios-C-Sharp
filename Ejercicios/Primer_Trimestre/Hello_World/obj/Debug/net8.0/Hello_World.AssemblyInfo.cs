c4b6fe5138c2ab97f801897210a363a987222e3d
Alberto Bollo <albertobollo7@protonmail.com>
2024-10-29 21:57:34 +0100

    Actualización ejercicios

--- Ejercicios/Primer_Trimestre/Hello_World/obj/Debug/net8.0/Hello_World.AssemblyInfo.cs
+++ Ejercicios/Primer_Trimestre/Hello_World/obj/Debug/net8.0/Hello_World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0783aa01cf82fd3854f908c6b1f953d7ac3424de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94f7bcd652bf584713bffc6758fef2a5fedc398")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
