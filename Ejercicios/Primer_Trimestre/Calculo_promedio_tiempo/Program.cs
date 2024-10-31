315d7880c970034a2df12b4245d6228272cd7b67
Alberto Bollo <albertobollo7@protonmail.com>
2024-10-31 07:36:19 +0100

    Nuevos ejercicios

---
+++ Ejercicios/Primer_Trimestre/Calculo_promedio_tiempo/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 1.-Hacer un programa que permita ingresar los kilómetros existentes  entre dos ciudades y la velocidad promedio de un vehículo. 
+ * Calcular y emitir por pantalla el tiempo aproximado que demandará  llegar de un punto a otro.
+ * La formula es :    tiempo = kilometros / velocidad
+ */
+
+Console.WriteLine("Introduce el kilometraje entre ciudad A y ciudad B: ");
+
+double kilometraje = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduce la velocidad promedia a la que crees que vas a conducir durante todo el trayecto: ");
+int velocidad = int.Parse(Console.ReadLine());
+
+double tiempo = kilometraje / velocidad;
+
+Console.WriteLine("El tiempo de trayecto aproximado es de: " + tiempo + " horas");
---
+++ Ejercicios/Primer_Trimestre/Ingresar_Notas/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 2.-Hacer un programa para ingresar por teclado las tres notas de exámenes de un alumno y luego calcule
+ * y emita por pantalla la media final(sumar las 3 y dividir entre 3)
+ */
+
+Console.WriteLine("Bienvenido a la calculadora de notas, a continuacón introduce las notas de los examenes para" +
+                  " calcular la media final");
+
+double nota1, nota2, nota3, notaMedia;
+
+Console.WriteLine("Introduce la nota del primer examen: ");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Introduce la nota del segundo examen: ");
+nota2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Introduce la nota del tercer examen: ");
+nota3 = double.Parse(Console.ReadLine());
+
+notaMedia = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine("La nota media final del alumno es: " + notaMedia);
