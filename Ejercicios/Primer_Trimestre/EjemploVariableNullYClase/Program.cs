be52a2dfd5302e476b01a1e13b026c0a86b5281a
Alberto Bollo <albertobollo7@protonmail.com>
2024-11-10 18:48:47 +0100

    Nuevos ejercicios

---
+++ Ejercicios/Primer_Trimestre/EjemploVariableNullYClase/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace EjemploVariableNullYClase
+{
+    public class EjemploVariableNullYClase
+    {
+        static void Main(string[] args)
+        {
+            // declaracion de variable con posibilidad de null
+            string? nombre;
+            string? apellidos = null;
+            // ejecución del programa
+            Console.WriteLine("Comenzando la ejecución del programa");
+            Console.WriteLine("Para comenzar por favor introduce tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Perfecto {0} ya podemos comenzar", nombre, apellidos);
+        }
+    }
+}
