5dc67a653fcc0a2b5d056162926f02421720a3b0
Alberto Bollo <albertobollo7@protonmail.com>
2024-11-10 19:01:32 +0100

    Nuevos ejercicios

---
+++ Ejercicios/Primer_Trimestre/EjemploMetodo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace EjemploMetodo
+{
+    class EjemploMetodo
+    {
+        static public void Main(String[] args)
+        {
+            realizarSuma(1, 7);
+            Console.WriteLine("El resultado de la multiplicación es {0}", realizarMultiplicacion(8, 2));
+        }
+
+        static public void realizarSuma(int op1, int op2)
+        {
+            Console.WriteLine("El resultado de la suma es {0}", op1 + op2);
+        }
+
+        static public int realizarMultiplicacion(int op1, int op2)
+        {
+            return op1 * op2;
+        }
+    }
+}
