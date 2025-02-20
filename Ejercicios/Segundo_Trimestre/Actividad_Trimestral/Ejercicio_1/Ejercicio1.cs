7afd91da5a34f8e20e2d3e06950eea9cdd4fe76b
Alberto Bollo <albertobollo7@protonmail.com>
2025-02-20 20:53:55 +0100

    Nuevos ejercicios

---
+++ Ejercicios/Segundo_Trimestre/Actividad_Trimestral/Ejercicio_1/Ejercicio1.cs
@@ -0,0 +1,56 @@
+﻿/*
+Escribe un programa que cree un array de 5 números enteros, solicite al usuario que
+ingrese los valores manualmente, y luego calcule y muestre la suma de todos los
+elementos del array.
+*/
+
+namespace Ejercicio_1
+{
+    
+    class Ejercicio1
+    {
+   
+        static void Main()
+        {
+            int [] arrayNumeros = new int [5];
+            Rellenar(arrayNumeros);
+            Console.WriteLine(SumaArray(arrayNumeros));
+
+        }
+
+        static int IntroducirNumeros()
+        {
+            int numeroEntero;
+            Console.WriteLine("Introduce un numero entero positivo");
+            numeroEntero = Convert.ToInt32(Console.ReadLine());
+            while (numeroEntero < 0)
+            {
+                Console.WriteLine("Error, introduce un numero entero positivo: ");
+                numeroEntero = Convert.ToInt32(Console.ReadLine());
+            }
+    
+            return numeroEntero;
+        }
+
+        static string SumaArray(int[] numeros)
+        {
+            int acumulador = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                acumulador += numeros[i];
+            }
+
+            return "La suma de todos los numeros del array es: " + acumulador;
+        }
+
+        static void Rellenar(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = IntroducirNumeros();
+            }
+        } 
+    }
+    
+}
---
+++ Ejercicios/Segundo_Trimestre/Actividad_Trimestral/Ejercicio_2/Ejercicio2.cs
@@ -0,0 +1,69 @@
+﻿/*
+Crea un programa que inicialice un array de 8 números enteros. Pide al usuario que
+ingrese un número para buscar en el array y muestra si el número está presente o no,
+indicando su posición si lo encuentra.
+
+*/
+
+namespace Ejercicio_2;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        int[] arrayNumeros = { 89, 23, 92, 76, 34, 65, 72, 71 };
+        MostrarArray(arrayNumeros);
+        int numeroBuscado = SolicitarNumero();
+        int posicionNumero = BuscarNumero(arrayNumeros, numeroBuscado);
+        while (posicionNumero == -1)
+        {
+            Console.WriteLine($"El numero introducido: {numeroBuscado} no se ha encontrado en el array" +
+                              $" prueba de nuevo");
+            numeroBuscado = SolicitarNumero();
+            posicionNumero = BuscarNumero(arrayNumeros, numeroBuscado);
+        }
+
+        MostarInformacion(numeroBuscado, posicionNumero);
+    }
+
+    static int SolicitarNumero()
+    {
+        int numeroBuscado;
+        Console.WriteLine("Introduce un numero entero positivo");
+        numeroBuscado = Convert.ToInt32(Console.ReadLine());
+        return numeroBuscado;
+    }
+
+    static int BuscarNumero(int[] arrayNumeros, int numeroBuscado)
+    {
+        for (int i = 0; i < arrayNumeros.Length; i++)
+        {
+            if (arrayNumeros[i] == numeroBuscado)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    static void MostarInformacion(int numeroBuscado, int posicionBuscada)
+    {
+        if (posicionBuscada != -1)
+        {
+            Console.WriteLine($"El numero introducido: {numeroBuscado} se ha encontrado en la posicion:" +
+                              $" {posicionBuscada}");
+        }
+    }
+
+    static void MostrarArray(int[] arrayNumeros)
+    {
+        Console.WriteLine("Mostrando todos los numeros del array: ");
+        for (int i = 0; i < arrayNumeros.Length; i++)
+        {
+            Console.Write(arrayNumeros[i] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
