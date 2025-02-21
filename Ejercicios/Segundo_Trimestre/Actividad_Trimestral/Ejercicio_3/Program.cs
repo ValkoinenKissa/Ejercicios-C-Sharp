4b8f80bcbca88a66a88c768919ec1875f8b826a3
Alberto Bollo <albertobollo7@protonmail.com>
2025-02-21 19:58:46 +0100

    Nuevos ejercicios

---
+++ Ejercicios/Segundo_Trimestre/Actividad_Trimestral/Ejercicio_3/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Desarrolla un programa que permita al usuario crear una lista de calificaciones. El
+programa debe:
+• Permitir ingresar calificaciones
+• Calcular el promedio de las calificaciones
+*/
+
+namespace Ejercicio_3;
+
+class Ejercicio3
+{
+    static void Main()
+    {
+
+        List<double> lista = new List<double>();
+        IngresarCalificaciones(lista);
+        double resultadoFinal = CalcularMediaCalificaciones(lista);
+        Console.WriteLine($"La media de la nota de los alumnos es: {resultadoFinal}");
+
+    }
+
+    static void IngresarCalificaciones(List<double> lista)
+    {
+        string decision;
+        double calificacion;
+        int contador = 1;
+        Console.WriteLine("Ingresa las calificaciones de tus alumnos, para continuar introduce 'Y' y para parar " +
+                          " introduce 'N' ");
+
+        do
+        {
+            Console.WriteLine($"Ingresa la calificación del alumno {contador}");
+            calificacion = Convert.ToDouble(Console.ReadLine());
+            if (calificacion < 0 || calificacion > 10)
+            {
+                Console.WriteLine("La calificacion introducida es incorrecta, debe de estar entre 0 y 10");
+                Console.WriteLine("¿Quieres continuar? (Y or N) ");
+                decision = Console.ReadLine()?.ToUpper() ?? "N"; // Si es null, usa "N" como default
+            }
+            else
+            {
+                lista.Add(calificacion);
+                Console.WriteLine("Calificación registrada correctamente");
+                Console.WriteLine("¿Quieres continuar? (Y or N) ");
+                decision = Console.ReadLine()?.ToUpper() ?? "N"; // Si es null, usa "N" como default
+                contador++;
+            }
+
+        } while (decision.Equals("Y"));
+        
+        Console.WriteLine("Procesando la media de las calificaciones obtenidas...");
+    }
+
+    static double CalcularMediaCalificaciones(List<double> lista)
+    {
+        double acumulador = 0;
+        int contadorElementos = lista.Count;
+        foreach (double valores in lista)
+        {
+            
+            acumulador += valores;
+            
+        }
+        
+        double mediaFinal = acumulador / contadorElementos;
+        return mediaFinal;
+    }
+    
+}
+
---
+++ Ejercicios/Segundo_Trimestre/Actividad_Trimestral/Ejercicio_4/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Crea un programa de lista de compras que permita:
+• Agregar elementos a la lista
+• Mostrar todos los elementos de la lista
+*/
+
+namespace Ejercicio_4;
+
+class Ejercicio4
+{
+    static void Main()
+    {
+        List<string> listaArticulos = new List<string>();
+        IngresarElementos(listaArticulos);
+        MostarArticulos(listaArticulos);
+    }
+    
+    static void IngresarElementos(List<string> lista)
+    {
+        string decision, articulo;
+        int contador = 1;
+        Console.WriteLine("Ingresa los articulos de tu lista de la compra, para continuar introduce 'Y'. Para parar " +
+                          " introduce 'N' ");
+
+        do
+        {
+            Console.WriteLine($"Ingresa el articulos Nº {contador}");
+            articulo = Console.ReadLine()?.ToUpper() ?? "NULL";
+            if (articulo.Equals("NULL"))
+            {
+                Console.WriteLine("No has introducido ningún articulo");
+                Console.WriteLine("¿Quieres continuar? (Y or N) ");
+                decision = Console.ReadLine()?.ToUpper() ?? "N"; // Si es null, usa "N" como default
+            }
+            else
+            {
+                lista.Add(articulo);
+                Console.WriteLine("Articulo registrado correctamente");
+                Console.WriteLine("¿Quieres continuar? (Y or N) ");
+                decision = Console.ReadLine()?.ToUpper() ?? "N"; // Si es null, usa "N" como default
+                contador++;
+            }
+
+        } while (decision.Equals("Y"));
+    }
+
+    static void MostarArticulos(List<string> lista)
+    {
+        Console.WriteLine("Mostrando los articulos de la compra...");
+        foreach (string articulo in lista)
+        {
+            Console.WriteLine(articulo);
+        }
+    }
+}
+
