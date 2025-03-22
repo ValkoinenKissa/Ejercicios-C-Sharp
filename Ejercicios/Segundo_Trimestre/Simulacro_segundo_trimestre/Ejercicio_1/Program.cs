f3366245707ee70f77a4133581a6ceaa7d5c017a
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-22 13:25:05 +0100

    Ejercicios simulacro

---
+++ Ejercicios/Segundo_Trimestre/Simulacro_segundo_trimestre/Ejercicio_1/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Escribe un programa en C# que solicite al usuario un número entero positivo. El programa debe verificar si el número
+es primo o no. Un número primo es aquel que solo es divisible por 1 y por sí mismo.
+Si el número no es válido (negativo o cero), el programa debe mostrar un mensaje de error.
+*/
+
+namespace Ejercicio_1;
+
+class NumerosPrimos
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Usuario, introduce un numero para verificar si es primo o no lo es: ");
+                int numeroIntroducido = Convert.ToInt32(Console.ReadLine());
+                if (ValidarNumero(numeroIntroducido))
+                {
+                    ComprobarNumeroPrimo(numeroIntroducido);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No puedes comprobar numeros que sean 0 o numeros menores que 0");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(
+                    "Solo se permite ingresar numeros, por favor no introduzcas nada que no sea un numero");
+            }
+        }
+    }
+
+    static void ComprobarNumeroPrimo(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero que has introducido es primo");
+        }
+        else
+        {
+            Console.WriteLine("El numero que has introducido no es primo");
+        }
+    }
+
+    static Boolean ValidarNumero(int numero)
+    {
+        if (numero <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Ejercicios/Segundo_Trimestre/Simulacro_segundo_trimestre/Ejercicio_2/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Escribe un programa en C# que permita al usuario ingresar una lista de números
+enteros separados por comas. El programa debe almacenar los números en una
+lista, calcular la suma de todos los elementos y mostrar tanto la lista como la
+suma total.
+*/
+
+namespace Ejercicio_2;
+
+class ListaNumeros
+{
+    public static void Main(string[] args)
+    {
+        List<String> listaNumeros = new List<String>();
+        List<int> listaNumerosClean = new List<int>();
+
+        Console.WriteLine("Introduce una serie de numeros separados por comas para cargar la lista");
+
+        do
+        {
+            Console.Write("Ingrese los numeros: ");
+            String numero = (Console.ReadLine() ?? "");
+            IngresarNumeros(listaNumeros, numero);
+            Console.WriteLine("Quieres continuar? (S o N)");
+            string continuar = (Console.ReadLine() ?? "").ToUpper();
+
+            if (continuar.Equals("N"))
+            {
+                Console.WriteLine("Mostrando todos los valores de la lista: ");
+                MostarNumeros(listaNumerosClean);
+                Console.WriteLine("Mostrando la suma total de valores: ");
+                SumaNumeros(listaNumeros, listaNumerosClean);
+                break;
+            }
+
+            if (continuar.Equals("S"))
+            {
+                Console.WriteLine("Has decidido continuar");
+            }
+            else
+            {
+                Console.WriteLine("Opcion no valida");
+            }
+        } while (true);
+    }
+
+
+    static void IngresarNumeros(List<String> listaNumeros, String numero)
+    {
+        Console.WriteLine("Ingresa los numeros que desees separados por comas: ");
+        listaNumeros.Add(numero);
+    }
+
+    static void SumaNumeros(List<String> listaNumeros, List<int> listaNumerosClean)
+    {
+        int acumulado = 0;
+        
+        foreach (String numeros in listaNumeros)
+        {
+            acumulado = int.Parse(numeros.Replace(",", ""));
+            listaNumerosClean.Add(acumulado);
+            acumulado += acumulado;
+        }
+
+        Console.WriteLine($"La suma total de valores del array es: {acumulado}");
+    }
+
+    static void MostarNumeros(List<int> listaNumerosClean)
+    {
+        foreach (int numeros in listaNumerosClean)
+        {
+            Console.Write(numeros);
+        }
+
+        Console.WriteLine();
+    }
+}
