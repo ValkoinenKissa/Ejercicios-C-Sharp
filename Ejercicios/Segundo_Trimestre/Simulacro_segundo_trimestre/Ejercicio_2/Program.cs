701b10cb1a0b5a52efb4d14b31c3103eabeff725
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-28 19:38:15 +0100

    Ejercicio simulacro corregido

--- Ejercicios/Segundo_Trimestre/Simulacro_segundo_trimestre/Ejercicio_2/Program.cs
+++ Ejercicios/Segundo_Trimestre/Simulacro_segundo_trimestre/Ejercicio_2/Program.cs
@@ -11,28 +11,27 @@ class ListaNumeros
 {
     public static void Main(string[] args)
     {
-        List<String> listaNumeros = new List<String>();
-
         Console.WriteLine("Introduce una serie de numeros separados por comas para cargar la lista");
-
         do
         {
             Console.Write("Ingrese los numeros: ");
             String numero = (Console.ReadLine() ?? "");
-            IngresarNumeros(listaNumeros, numero);
             Console.WriteLine("Quieres continuar? (S o N)");
             string continuar = (Console.ReadLine() ?? "").ToUpper();
 
-            if (continuar.Equals("N"))
+            if (continuar.Equals("N") && numero.Contains(','))
             {
                 Console.WriteLine("Mostrando todos los valores de la lista: ");
-                MostarNumeros(listaNumeros);
+                MostarNumeros(numero);
                 Console.WriteLine("Mostrando la suma total de valores: ");
-                SumaNumeros(listaNumeros);
+                SumaNumeros(numero);
                 break;
             }
-
-            if (continuar.Equals("S"))
+            else if (continuar.Equals("N") && !numero.Contains(','))
+            {
+                Console.WriteLine("Error: Debes de separar los numeros con una coma (,) Ejemplo: 5,6,7,8,9,10");
+            }
+            else if (continuar.Equals("S"))
             {
                 Console.WriteLine("Has decidido continuar");
             }
@@ -43,33 +42,21 @@ class ListaNumeros
         } while (true);
     }
 
-
-    static void IngresarNumeros(List<String> listaNumeros, String numero)
-    {
-        Console.WriteLine("Ingresa los numeros que desees separados por comas: ");
-        listaNumeros.Add(numero);
-    }
-
-    static void SumaNumeros(List<String> listaNumeros)
+    static void SumaNumeros(String listaNumeros)
     {
         int acumulado = 0;
-        String numeroClean;
-        
-        foreach (String numeros in listaNumeros)
+        String[] numeroClean = listaNumeros.Split(',');
+
+        foreach (var numero in numeroClean)
         {
-            numeroClean = numeros.Replace(",", " ");
-            acumulado += int.Parse(numeroClean);
+            acumulado += Convert.ToInt32(numero);
         }
 
         Console.WriteLine($"La suma total de valores del array es: {acumulado}");
     }
 
-    static void MostarNumeros(List<String> listaNumeros)
+    static void MostarNumeros(String listaNumeros)
     {
-        foreach (String numeros in listaNumeros)
-        {
-            Console.Write(numeros);
-        }
-        Console.WriteLine();
+        Console.WriteLine(listaNumeros);
     }
 }
