35cfa0dc2d366242fd7f4ed9634eb55f800d767f
Alberto Bollo <albertobollo7@protonmail.com>
2025-04-07 07:57:32 +0200

    Actividad trimestral 3

---
+++ Ejercicios/Tercer_Trimestre/Actividad_trimestral/Actividad_Trimestral_3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Escribe un programa en C# que lea los datos de un fichero de texto llamado empleados.txt. El
+fichero contiene información sobre empleados, donde cada línea representa a un empleado y los
+campos están separados por el carácter :. Los campos son los siguientes:
+
+
+1. ID: Identificador único del empleado (entero).
+2. Nombre: Nombre completo del empleado (cadena).
+3. Edad: Edad del empleado (entero).
+4. Departamento: Departamento al que pertenece el empleado (cadena).
+5. Salario: Salario mensual del empleado (decimal).
+
+
+
+El programa debe realizar las siguientes tareas:
+1. Leer el contenido del fichero y almacenar los datos en una lista de objetos.
+2. Mostrar por consola todos los empleados con su información formateada.
+3. Calcular y mostrar el salario promedio de todos los empleados.
+4. Filtrar y mostrar los empleados cuyo salario sea mayor al promedio.
+
+
+Ejemplo del fichero de texto (empleados.txt)
+1:Juan Pérez:30:Ventas:2500.50
+2:Ana Gómez:25:Marketing:3000.00
+3:Carlos Ruiz:40:IT:4500.75
+4:Sofía López:35:Recursos Humanos:3200.25
+5:Luis Martínez:28:Ventas:2800.00
+*/
+
+namespace Actividad_Trimestral_3;
+
+class Empleados
+{
+
+    static void Main()
+    {
+        
+    }
+    
+}
+
