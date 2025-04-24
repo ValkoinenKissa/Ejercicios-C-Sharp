3dee1d6d38af5c7e50d6876ac52cc4893ecdc937
Alberto Bollo <albertobollo7@protonmail.com>
2025-04-24 23:19:48 +0200

    Ejercicios conex bbdd

--- Ejercicios/Primer_Trimestre/Actividad_Condicionales_II/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
+++ Ejercicios/Primer_Trimestre/Actividad_Condicionales_II/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeab39c5d2399ae14681dd3bd41be05d5817d1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69eb1818d1bd3b9a0d669e18302bd8c658fd8b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Ejercicios/Tercer_Trimestre/Ejercicios_sql/Delete/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Delete
+{
+    using MySql.Data.MySqlClient;
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Server=localhost;Database=Tienda;Uid=root;Pwd=rootroot;";
+
+            Console.WriteLine("-- ELIMINAR PRODUCTO --");
+            Console.Write("ID del producto a eliminar: ");
+            int id = int.Parse(Console.ReadLine() ?? "");
+
+            using (MySqlConnection conexion = new MySqlConnection(connectionString))
+            {
+                conexion.Open();
+                string sql = "DELETE FROM productos WHERE Id = @id";
+                MySqlCommand cmd = new MySqlCommand(sql, conexion);
+                cmd.Parameters.AddWithValue("@id", id);
+            
+                int filasAfectadas = cmd.ExecuteNonQuery();
+                Console.WriteLine(filasAfectadas > 0 ? "¡Producto eliminado!" : "❌ Producto no encontrado");
+            }
+        }
+    }
+
+}
