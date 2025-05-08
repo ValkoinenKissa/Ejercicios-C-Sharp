19fe34767769c7361423b4c531a8dadcbf5ae5fa
Alberto Bollo <albertobollo7@protonmail.com>
2025-05-08 19:46:02 +0200

    ArrayList examples were added.

---
+++ Ejercicios/Segundo_Trimestre/Ejercicios_ficheros/Ejercicio_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Ejercicio 1: Añadir elementos a una lista
+Enunciado:
+Crea un programa en C# que permita al usuario añadir 5 nombres a una lista y luego los muestre por consola.
+*/
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> nombres = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Introduce el nombre {i + 1}:");
+                string nombre = Console.ReadLine() ?? ""; //Operador de sustitucion para evitar nulos
+                nombres.Add(nombre);
+            }
+
+            Console.WriteLine("Los nombres introducidos son:");
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
---
+++ Ejercicios/Segundo_Trimestre/Ejercicios_listas/Ejercicio_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Ejercicio 3: Buscar un elemento en la lista
+Enunciado:
+Escribe un programa que permita al usuario buscar un número en una lista y diga si está presente.
+ */
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numeros = new List<int> { 5, 15, 25, 35, 45 };
+            Console.WriteLine("Introduce un número para buscar:");
+            int numeroBuscado = Convert.ToInt32(Console.ReadLine());
+
+            if (numeros.Contains(numeroBuscado))
+            {
+                Console.WriteLine("El número está en la lista.");
+            }
+            else
+            {
+                Console.WriteLine("El número no está en la lista.");
+            }
+        }
+    }
+}
